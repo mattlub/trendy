f288c9dd39893efa5e1e72dc4fe57cccfca15d7a
matt lubel <matt_lubel@hotmail.com>
2017-10-23 12:49:26 +0100

    move test fixtures

---
+++ tests/fixtures.js
@@ -0,0 +1,20 @@
+const repos = [
+  {
+    id: 123456,
+    name: 'test',
+    description: 'cool new library',
+    html_url: 'https://www.github.com',
+    stargazers_count: 103,
+    language: 'JavaScript'
+  },
+  {
+    id: 333444,
+    name: 'another one',
+    description: 'cool newer library',
+    html_url: 'https://www.github.com',
+    stargazers_count: 300,
+    language: 'Elm'
+  }
+]
+
+module.exports = { repos }
