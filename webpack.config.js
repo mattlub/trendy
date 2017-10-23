a6a2cd73394d41b87386d7fe4f140373f0e1f3c3
matt lubel <matt_lubel@hotmail.com>
2017-10-23 10:01:35 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: './public/bundle.js'
+  },
+  module: {
+    loaders: [
+      {  
+        test: /\/src\/(.*)\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ],
+  }
+}
